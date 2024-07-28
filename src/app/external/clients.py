7c6a3c7a7c52ca0c3f7e52621048bf1f9f4552bc
GeorgiyKuzora <rafale87@gmail.com>
2024-07-28 11:02:23 +0000

    Обновляет и исправляет клиенты clients.py

--- src/app/external/clients.py
+++ src/app/external/clients.py
@@ -1,10 +1,11 @@
 import logging
 from collections import namedtuple
-from typing import Annotated
+from dataclasses import dataclass
+from enum import StrEnum
+from typing import Annotated, Any
 
-import aiohttp
-from fastapi import Depends, Header, status
-from fastapi.exceptions import HTTPException
+import httpx
+from fastapi import Header, status
 
 from app.api.models import (
     Report,
@@ -13,18 +14,50 @@ from app.api.models import (
     Transaction,
     UserCredentials,
 )
+from app.system import errors
+from config.config import get_settings
 
 logger = logging.getLogger(__name__)
-session = aiohttp.ClientSession()
+
+good_response = {'message': 'ok'}
+
+
+class Key(StrEnum):
+    """Часто повторяемые ключи."""
+
+    encoded_token = 'encoded_token'  # noqa: S105 key
+    authorization = 'Authorization'
+    transactions = 'transactions'
+
+
+@dataclass
+class Client:
+    """Клиент библиотеки для формирования HTTP запросов."""
+
+    client: httpx.AsyncClient = httpx.AsyncClient()
+
+    async def get(self, *args, **kwargs) -> Any:
+        """Метод GET."""
+        async with self.client as client:
+            response = await client.get(*args, **kwargs)
+        return response
+
+    async def post(self, *args, **kwargs) -> Any:
+        """Метод POST."""
+        async with self.client as client:
+            response = await client.post(*args, **kwargs)
+        return response
 
 
 class AuthServiceClient:
     """Клиент для доступа к сервису аутентификации."""
 
-    def __init__(self) -> None:
+    def __init__(self, client: Client) -> None:
         """Метод инициализации."""
-        self.host = 'host'
-        self.port = 'port'
+        settings = get_settings()
+        self.host = settings.auth_host
+        self.port = settings.auth_port
+        self.client = client
 
     async def register(self, user_creds: UserCredentials) -> Token:
         """
@@ -34,20 +67,19 @@ class AuthServiceClient:
         :type user_creds: UserCredentials
         :return: Токен пользователя
         :rtype: Token
-        :raises HTTPException: В случае плохого ответа сервиса
+        :raises ServerError: В случае плохого ответа сервиса
         """
-        async with session.post(
-            f'{self.host}:{self.port}', json=user_creds.model_dump_json(),
-        ) as resp:
-            if resp.status == status.HTTP_500_INTERNAL_SERVER_ERROR:
-                logger.error(
-                    f'{self.host}:{self.port} respose status {status.HTTP_500_INTERNAL_SERVER_ERROR}',  # noqa
-                )
-                raise HTTPException(
-                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                )
-            payload = await resp.json()
-            return Token(token=payload.get('token'))
+        url = f'{self.host}:{self.port}/register'
+        payload = user_creds.model_dump_json()
+        resp = await self.client.post(
+            url, json=payload,
+        )
+        if resp.status_code != status.HTTP_200_OK:
+            logger.error(
+                f'{url} {status.HTTP_500_INTERNAL_SERVER_ERROR}',  # noqa: E501
+            )
+            raise errors.ServerError()
+        return Token(token=resp.json().get(Key.encoded_token, ''))
 
     async def authenticate(
         self,
@@ -63,38 +95,35 @@ class AuthServiceClient:
         :type authorization: str
         :return: Токен пользователя
         :rtype: Token
-        :raises HTTPException: В случае плохого ответа сервиса
+        :raises NotFoundError: данные не найдены
+        :raises UnauthorizedError: неавторизирован
+        :raises ServerError: ошибка сервера
         """
-        headers = {'Authorization:': authorization}
-        async with session.post(
-            f'{self.host}:{self.port}',
+        url = f'{self.host}:{self.port}/login'
+        headers = {Key.authorization: authorization}
+        resp = await self.client.post(
+            url,
             json=user_creds.model_dump_json(),
             headers=headers,
-        ) as resp:
-            if resp.status == status.HTTP_401_UNAUTHORIZED:
-                logger.error(
-                    f'{self.host}:{self.port} respose status {status.HTTP_401_UNAUTHORIZED}',  # noqa
-                )
-                raise HTTPException(
-                    status_code=status.HTTP_401_UNAUTHORIZED,
-                )
-            elif resp.status == status.HTTP_404_NOT_FOUND:
-                logger.error(
-                    f'{self.host}:{self.port} respose status {status.HTTP_404_NOT_FOUND}',  # noqa
-                )
-                raise HTTPException(
-                    status_code=status.HTTP_404_NOT_FOUND,
-                )
-            elif resp.status == status.HTTP_200_OK:
-                payload = await resp.json()
-                return Token(token=payload.get('token'))
-
+        )
+        if resp.status_code == status.HTTP_401_UNAUTHORIZED:
             logger.error(
-                f'{self.host}:{self.port} respose status {status.HTTP_500_INTERNAL_SERVER_ERROR}',  # noqa
+                f'{url} {status.HTTP_401_UNAUTHORIZED}',
             )
-            raise HTTPException(
-                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            raise errors.UnauthorizedError()
+        elif resp.status_code == status.HTTP_404_NOT_FOUND:
+            logger.error(
+                f'{url} {status.HTTP_404_NOT_FOUND}',
             )
+            raise errors.NotFoundError()
+        elif resp.status_code == status.HTTP_200_OK:
+            payload = resp.json()
+            return Token(token=payload.get(Key.encoded_token))
+
+        logger.error(
+            f'{url} {status.HTTP_500_INTERNAL_SERVER_ERROR}',
+        )
+        raise errors.ServerError()
 
     async def check_token(
         self, authorization: Annotated[str, Header()],
@@ -104,35 +133,38 @@ class AuthServiceClient:
 
         :param authorization: Заголовок с токеном пользователя
         :type authorization: str
-        :raises HTTPException: Ошибка доступа
+        :raises NotFoundError: данные не найдены
+        :raises UnauthorizedError: неавторизирован
+        :raises ServerError: ошибка сервера
         """
-        headers = {'Authorization:': authorization}
-        async with session.post(
-            f'{self.host}:{self.port}',
+        url = f'{self.host}:{self.port}/check_token'
+        headers = {Key.authorization: authorization}
+        resp = await self.client.post(
+            url=url,
             headers=headers,
-        ) as resp:
-            if resp.status == status.HTTP_404_NOT_FOUND:
-                logger.info('токен не найден')
-                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-            elif resp.status == status.HTTP_401_UNAUTHORIZED:
-                logger.info('токен не валиден')
-                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED1)
-            elif resp.status == status.HTTP_200_OK:
-                logger.debug('токен валиден')
-
-            logger.error('неизвестная ошибка сервера')
-            raise HTTPException(
-                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            )
+        )
+        if resp.status_code == status.HTTP_404_NOT_FOUND:
+            logger.info(f'{url} токен не найден')
+            raise errors.NotFoundError()
+        elif resp.status_code == status.HTTP_401_UNAUTHORIZED:
+            logger.info(f'{url} токен не валиден')
+            raise errors.UnauthorizedError()
+        elif resp.status_code == status.HTTP_200_OK:
+            logger.debug(f'{url}токен валиден')
+        else:
+            logger.error(f'{url} ошибка сервера')
+            raise errors.ServerError()
 
 
 class TransactionServiceClient:
     """Клиент сервиса транзакций."""
 
-    def __init__(self) -> None:
+    def __init__(self, client: Client) -> None:
         """Метод инициализации."""
-        self.host = 'host'
-        self.port = 'port'
+        settings = get_settings()
+        self.host = settings.transactions_host
+        self.port = settings.transactions_port
+        self.client = client
 
     async def get_report(
         self,
@@ -145,46 +177,60 @@ class TransactionServiceClient:
         :type report_request: ReportRequest
         :return: Отчет о транзакциях
         :rtype: Report
-        :raises HTTPException: Ошибка доступа
+        :raises ServerError: Ошибка доступа
         """
-        async with session.get(
-            f'{self.host}:{self.port}',
+        url = f'{self.host}:{self.port}/create_report'
+        resp = await self.client.post(
+            url=url,
             json=report_request.model_dump_json(),
-        ) as resp:
-            if resp.status == status.HTTP_200_OK:
-                payload: dict = await resp.json()
-                request = payload.get('request')
-                transactions = payload.get('transactions')
-                if request is not None and transactions is not None:
-                    request = ReportRequest(**request)
-                    report = Report(request=request, transactions=transactions)
-                else:
-                    logger.error('Ошибка при получении отчета')
-                    raise HTTPException(
-                        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                    )
-                logger.debug(f'Отчет получен: {report}')
-                return report
+        )
+        if resp.status_code == status.HTTP_200_OK:
+            payload: dict[str, str | int] = resp.json()
+            return self._make_report(payload, report_request)
 
-            logger.error('неизвестная ошибка сервера')
-            raise HTTPException(
-                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            )
+        logger.error(f'{url} ошибка сервера')
+        raise errors.ServerError()
 
     async def create_transaction(
         self, transaction: Transaction,
     ) -> dict[str, str]:
-        async with session.post(
-            f'{self.host}:{self.port}',
+        """
+        Создает транзакцию на основании данных пользователя.
+
+        :param transaction: Транзакция совершенная пользовалетем.
+        :type transaction: Transaction
+        :return: Сообщение о успехе
+        :rtype: dict[str, str]
+        :raises ServerError: При плохом ответе от сервиса
+        """
+        url = f'{self.host}:{self.port}/create_transaction'
+        resp = await self.client.post(
+            url=url,
             json=transaction.model_dump_json(),
-        ) as resp:
-            if resp.status == status.HTTP_201_CREATED:
-                logger.debug(f'транзакция создана: {transaction}')
-                return {'message': 'transaction created'}
-            logger.error('Ошибка при получении отчета')
-            raise HTTPException(
-                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            )
+        )
+        if resp.status_code == status.HTTP_201_CREATED:
+            logger.debug(f'транзакция создана: {transaction}')
+            return good_response
+        logger.error(f'{url} ошибка при создании транзации')
+        raise errors.ServerError()
+
+    def _make_report(self, payload, request) -> Report:
+        trasactions = self._make_transactions(payload.get(Key.transactions))
+        report = Report(
+            request=request, transactions=trasactions,
+        )
+        logger.debug(f'Отчет получен: {report}')
+        return report
+
+    def _make_transactions(
+        self, transactions: list[dict] | None,  # type: ignore
+    ) -> list[Transaction]:
+        if transactions is None:
+            raise ValueError('expected list but recieved None')
+        result_transactions = []
+        for key_value in transactions:
+            result_transactions.append(Transaction(**key_value))
+        return result_transactions
 
 
 Clients = namedtuple(
@@ -193,6 +239,6 @@ Clients = namedtuple(
 )
 
 clients = Clients(
-    auth_client=AuthServiceClient(),
-    transactions_client=TransactionServiceClient(),
+    auth_client=AuthServiceClient(Client()),
+    transactions_client=TransactionServiceClient(Client()),
 )
