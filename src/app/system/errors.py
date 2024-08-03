89a06e2ac239e6564f8e60d98be5304eb3bb6a76
GeorgiyKuzora <rafale87@gmail.com>
2024-08-03 13:21:37 +0000

    Добавляет функцию handle_stauts_codes в errors.py

--- src/app/system/errors.py
+++ src/app/system/errors.py
@@ -1,4 +1,8 @@
+import logging
+
 from fastapi import HTTPException, status
+
+logger = logging.getLogger(__name__)
 
 
 class ServerError(HTTPException):
@@ -6,7 +10,7 @@ class ServerError(HTTPException):
 
     def __init__(
         self,
-        status_code: int = status.HTTP_500_INTERNAL_SERVER_ERROR,
+        status_code: int = status.HTTP_503_SERVICE_UNAVAILABLE,
         detail: str = 'Неизвестная ошибка сервера',
     ):
         """
@@ -79,3 +83,23 @@ class UnprocessableError(HTTPException):
         """
         self.status_code = status_code
         self.detail = detail
+
+
+http_errors = {
+    status.HTTP_422_UNPROCESSABLE_ENTITY: UnprocessableError,
+    status.HTTP_404_NOT_FOUND: NotFoundError,
+    status.HTTP_503_SERVICE_UNAVAILABLE: ServerError,
+    status.HTTP_500_INTERNAL_SERVER_ERROR: ServerError,
+    status.HTTP_401_UNAUTHORIZED: UnauthorizedError,
+}
+
+good_status_codes = [status.HTTP_200_OK, status.HTTP_201_CREATED]
+
+
+def handle_status_code(status_code: int):
+    """Проверяет код ответа поднимает исключение при плохом коде ответа."""
+    if status_code not in good_status_codes:
+        logger.error(
+            f'Recived bad status code {status_code}',
+        )
+        raise http_errors.get(status_code, ServerError)()
